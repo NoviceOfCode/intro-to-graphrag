4fdcf6123669f72199e26915f5dd4c21726fe057
Jacob Xayaphet <jxcdx@umsystem.edu>
2025-01-05 16:05:38 -0600

    Finished tutorial
    Finished tutorial, but for some reason openai couldn't be completed

--- utils.py
+++ utils.py
@@ -2,36 +2,7 @@
 # Environment variables for NEO4J_URI (usually `bolt://localhost:7687` ), NEO4J_PASSWORD, NEO4J_USERNAME, and OPENAI_API_KEY should be set.
 # The OpenAI API key can be obtained from the OpenAI website.
 # The Neo4j URI, username, and password can be obtained from the Neo4j Desktop or Aura console.
-import os
-from dotenv import load_dotenv
 
-# def get_neo4j_uri():
-#     load_dotenv()
-#     uri = os.environ.get("NEO4J_URI")
-#     if not uri:
-#         raise ValueError("NEO4J_URI environment variable not set.")
-#     return uri
-#
-# def get_neo4j_username():
-#     load_dotenv()
-#     username = os.environ.get("NEO4J_USERNAME")
-#     if not username:
-#         raise ValueError("NEO4J_USERNAME environment variable not set.")
-#     return username
-#
-# def get_neo4j_password():
-#     load_dotenv()
-#     password = os.environ.get("NEO4J_PASSWORD")
-#     if not password:
-#         raise ValueError("NEO4J_PASSWORD environment variable not set.")
-#     return password
-#
-# def get_openai_client():
-#     load_dotenv()
-#     api_key = os.environ.get("OPENAI_API_KEY")
-#     if not api_key:
-#         raise ValueError("OPENAI_API_KEY environment variable not set.")
-#     return OpenAI(api_key=api_key)
 
 import os
 from dotenv import load_dotenv
